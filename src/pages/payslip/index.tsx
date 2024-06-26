bb97779f0158567b60281ce5e4b19a2b31094df7
lucasnbat <lucasnunesb6@gmail.com>
2024-06-26 08:35:38 -0400

    add: animação para mostrar quando arquivo está anexado ou não

--- src/pages/payslip/index.tsx
+++ src/pages/payslip/index.tsx
@@ -2,9 +2,31 @@ import Head from 'next/head';
 import styles from './styles.module.scss';
 import { canSSRAuth } from '@/src/utils/canSSRAuth';
 import { Header } from '@/src/components/Header';
-import { FiUpload } from 'react-icons/fi';
+import { FiCheckCircle, FiUpload } from 'react-icons/fi';
+import { ChangeEvent, useState } from 'react';
+import { toast } from 'react-toastify';
 
 export default function Payslip() {
+    const [docUrl, setDocUrl] = useState(''); // url da foto
+    const [docFile, setDocFile] = useState<File | null>(null); // foto em si
+
+    function handleFile(e: ChangeEvent<HTMLInputElement>) {
+        console.log(e.target.files);
+
+        if (!e.target.files) return;
+
+        const doc = e.target.files[0];
+
+        if (!doc) return;
+
+        // pode adicionar validação de tipo de doc aqui
+
+        setDocFile(doc)
+        setDocUrl(URL.createObjectURL(e.target.files[0]))
+
+        toast.success('Arquivo anexado!');
+    }
+
     return (
         <>
             <Head>
@@ -30,14 +52,31 @@ export default function Payslip() {
 
                         <label className={styles.labelDoc}>
                             <span>
-                                <FiUpload
-                                    size={25}
-                                    color='#fff'
-                                />
+                                {docFile ? (
+                                    <div>
+                                        <FiCheckCircle
+                                            size={25}
+                                            color='#fff'
+                                        />
+                                        <div>Anexado</div>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <FiUpload
+                                            size={25}
+                                            color='#fff'
+                                        />
+                                        <div>Insira um arquivo</div>
+                                    </div>
+                                )}
+
                             </span>
+
                             <input
                                 type="file"
                                 accept=".pdf, .docx, .odt"
+                                // value={docFile}
+                                onChange={handleFile}
                             />
                         </label>
 
