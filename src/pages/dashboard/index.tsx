13f0917ccc6927b8d79e10de3b46c8a47ef1026f
lucasnbat <lucasnunesb6@gmail.com>
2024-06-26 15:01:21 -0400

    componente holerite

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@/src/components/Header"
 import { canSSRAuth } from "@/src/utils/canSSRAuth"
 import Head from "next/head"
+import styles from './styles.module.scss'
+import { FiRefreshCcw } from "react-icons/fi"
 
 export default function Dashboard() {
     return (
@@ -12,7 +14,26 @@ export default function Dashboard() {
             <div>
                 <Header />
 
-                <h1>Painel</h1>
+                <main className={styles.container}>
+
+                    <div className={styles.containerHeader}>
+                        <h1>Últimos holerites</h1>
+                        <button>
+                            <FiRefreshCcw color='#3fffa3' size={25} />
+                        </button>
+                    </div>
+
+                    <article className={styles.listPayslips}>
+
+                        <section className={styles.payslipItem}>
+                            <button>
+                                <div className={styles.tag}></div>
+                                <span>Holerite 37289</span>
+                            </button>
+                        </section>
+                        
+                    </article>
+                </main>
             </div>
         </>
     )
