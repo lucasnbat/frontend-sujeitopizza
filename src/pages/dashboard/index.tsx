ac10bcc115abea902480d146c019f4fe4f6ada3f
lucasnbat <lucasnunesb6@gmail.com>
2024-07-01 11:10:24 -0400

    (dashboard)fix: ajustes no dashboard para buscar as publishes

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,8 +4,30 @@ import Head from "next/head";
 import styles from "./styles.module.scss";
 import { FiEdit, FiRefreshCcw, FiSend, FiUpload } from "react-icons/fi";
 import Link from "next/link";
+import { use, useEffect, useState } from "react";
+import { api } from "@/src/services/apiClient";
+
+interface PublishReturn {
+    id: string;
+    observation: string;
+    competenciaPublish: string;
+}
 
 export default function Dashboard() {
+    const [publishes, setPublishes] = useState([]);
+
+    useEffect(() => {
+        async function loadPublishes() {
+            const response = await api.get("/getAllPublishes");
+
+            setPublishes(response.data);
+        }
+
+        loadPublishes();
+    }, []);
+
+    console.log(publishes)
+
     return (
         <>
             <Head>
@@ -24,7 +46,7 @@ export default function Dashboard() {
                     </div>
 
                     <article className={styles.listPayslips}>
-                        <section className={styles.payslipItem}>
+                        {/* <section className={styles.payslipItem}>
                             <button>
                                 <div className={styles.tag}></div>
                                 <span>ID</span>
@@ -48,7 +70,38 @@ export default function Dashboard() {
                                     </span>
                                 </span>
                             </button>
-                        </section>
+                        </section> */}
+                        {publishes.map((publish: PublishReturn) => {
+                            return (
+                                <section key={publish.id} className={styles.payslipItem}>
+                                    <button className={styles.buttonStyle}>
+                                        <div className={styles.tag}></div>
+                                        <span className={styles.spanIdStyle}>{publish.id}</span>
+                                        <span className={styles.spanStatus}>
+                                            Status
+                                        </span>
+                                        <span className={styles.competenciaStyle}>{publish.competenciaPublish}</span>
+
+                                        <span className={styles.actions}>
+                                            <span>
+                                                <Link href="/payslip">
+                                                    <FiEdit size={19} />
+                                                </Link>
+                                            </span>
+                                            <span>
+                                                <Link href="/payslip">
+                                                    <FiUpload size={19} />
+                                                </Link>
+                                            </span>
+
+                                            <span>
+                                                <FiSend size={19} />
+                                            </span>
+                                        </span>
+                                    </button>
+                                </section>
+                            );
+                        })}
                     </article>
                 </main>
             </div>
